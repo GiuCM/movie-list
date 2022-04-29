05c8ea1fb90036c3b54e050575b89df79e6f57c6
GiuCM <giuliana.campregher27@gmail.com>
2022-04-29 15:45:03 -0300

    calling the movie database api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,27 +2,24 @@ import { Container, MovieList, Movie } from "./styles";
 import { useEffect, useState } from "react";
 
 function Home() {
-	const test = process.env.REACT_APP_API_KEY
-
-	console.log(test)
-	console.log("oi")
-
 	const [movies, setMovies] = useState([])
+	const image_path = 'https://image.tmdb.org/t/p/w500'
 
 	useEffect(() => {
 		fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+			.then(response => response.json())
+			.then(data => setMovies(data.results))
 	}, [])
 
 	return (
 		<Container>
 			<h1>Movies</h1>
 			<MovieList>
-
 				{movies.map(movie => {
 					return (
 						<Movie key={movie.id}>
 							<a href="https://google.com.br">
-								<img src={movie.image_url} alt={movie.title} />
+								<img src={`${image_path}${movie.poster_path}`} alt={movie.title} />
 							</a>
 							<span>{movie.title}</span>
 						</Movie>
